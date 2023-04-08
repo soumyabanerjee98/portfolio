75954ed05bbae30ca9991921afd5a93ab188c77a
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-04-09 02:18:33 +0530

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         <title>{webConfig?.full_name}</title>
         <meta name="description" content={webConfig?.details} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content={`${webConfig?.url}metaimage.jpeg`} />
         <link rel="icon" href="/code.png" />
       </Head>
       <main className={styles.main}>
